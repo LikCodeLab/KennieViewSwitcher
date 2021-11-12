19ff403de086859d68933bed9ba2ac0b85dd791b
kennie <xdpvxv@163.com>
2021-11-12 13:53:48 +0800

    Initial commit

---
+++ library/src/main/java/com/kennie/library/Test.java
@@ -0,0 +1,17 @@
+package com.kennie.library;
+
+/**
+ * 测试类
+ */
+public class Test {
+
+    private String name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
